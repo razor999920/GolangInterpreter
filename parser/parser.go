b2bbf7b4fbbd818cea1a12f6405acfca099dea7a
razor999920 <raza9999ali@gmail.com>
2024-02-02 19:53:06 -0700

    adding parser helpers

--- parser/parser.go
+++ parser/parser.go
@@ -1,3 +1,35 @@
 package parser
 
+import (
+	"monkey/token"
+	"monkey/lexer"
+	"monkey/ast"
+)
+
+type Parser struct {
+	l *lexer .Lexer
+	
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l:l}
+
+	// Read two tokens so both curToken & peekToken are set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
+
 
