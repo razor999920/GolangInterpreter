9fb18cb454235aa4de3769aa2fa142b03d3896ed
razor999920 <raza9999ali@gmail.com>
2024-03-31 17:26:41 -0600

    adding test cases for infix boolean expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -27,7 +27,6 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"3 * 3 * 3 + 10", 37},
 		{"3 * (3 * 3) + 10", 37},
 		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
-
 	}
 
 	for _, tt := range tests {
@@ -66,6 +65,14 @@ func TestEvalBooleanExpression(t *testing.T) {
 	} {
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
 	}
 
 	for _, tt := range tests {
