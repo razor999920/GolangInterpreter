4ba2d2d67040dc6ab38d732d8d3ce06dfba2c61d
razor999920 <raza9999ali@gmail.com>
2024-04-27 21:37:22 -0600

    evaluating string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -221,6 +221,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -315,6 +319,20 @@ func TestStringLiteral(t *testing.T) {
 	str, ok := evaluated.(*object.String)
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 
 	if str.Value != "Hello World!" {
